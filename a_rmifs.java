89706eea6a8217c2cd7967fde26e2340d17dd1ef
Oriana Gomez <oriana@OrianaPC>
2014-01-06 19:00:49 -0430

    cambiando nombres

---
+++ a_rmifs.java
@@ -0,0 +1,88 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.*;
+
+import Clases.*;
+
+public class AutenticadorServer {
+
+  public AutenticadorServer (int puerto){
+    try{
+      LocateRegistry.createRegistry(puerto);
+      Autenticador a = new AutenticadorImpl();
+      Naming.rebind("rmi://127.0.0.1:"+puerto+"/AutenticadorService", a);
+    }
+    catch(Exception e){
+      System.out.println("Error: " + e);
+    }
+  }
+
+  private static void leerUsuariosEnArchivo(String arch)
+  throws java.rmi.RemoteException {
+    File archivo = null;
+    FileReader fr = null;
+    BufferedReader br = null;
+    String [] parUserPass;
+    String linea;
+
+    Autenticador a = new AutenticadorImpl();
+
+    try{
+      archivo = new File (arch);
+      fr = new FileReader (archivo);
+      br = new BufferedReader(fr);
+      while((linea=br.readLine())!=null){
+        parUserPass = linea.split(":");
+        Usuario u = new Usuario(parUserPass[0],parUserPass[1]);
+        a.guardar(u);
+      }
+    }
+    catch(Exception e){
+      System.out.println("Error leyendo archivo: " + e);
+    }
+    finally {
+     try{
+      if(fr!=null)
+        fr.close();
+      }
+      catch (Exception e2){
+        System.out.println("Error cerrando archivo: " + e2);
+      }
+    }
+  }
+
+  public static void main(String args[])
+  throws java.rmi.RemoteException {
+    String puerto;
+    if (args.length == 4) {
+      if (args[0] == "-f") {
+        System.out.println("entreee");
+        leerUsuariosEnArchivo(args[1]);
+        if (args[2] == "-p") {
+          puerto = args[3];
+          System.out.println("Puerto: "+puerto+"\n");
+        } else {
+          System.out.println("Error de sintaxis: a_rmifs -f usuarios -p puerto");
+          System.exit(0);
+        }
+      } else if (args[0] == "-p") {
+        System.out.println("ENTREEE");
+        puerto = args[1];
+        System.out.println("Puerto: "+args[1]+"\n");
+        if (args[2] == "-f")
+          leerUsuariosEnArchivo(args[3]);
+        else {
+          System.out.println("Error de sintaxis: a_rmifs -f usuarios -p puerto");
+          System.exit(0);
+        }
+      }
+    } else {
+        System.out.println("Error de sintaxis: a_rmifs -f usuarios -p puerto");
+        System.exit(0);
+    }
+    new AutenticadorServer(21131);
+  }
+}
---
+++ s_rmifs.java
@@ -0,0 +1,70 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+
+import Clases.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+
+public class ArchivosClienteServidor{
+
+  public ArchivosClienteServidor (int puerto){
+    try{
+      LocateRegistry.createRegistry(puerto);
+      Solicitud s = new SolicitudImpl();
+      Naming.rebind("rmi://127.0.0.1:"+puerto+"/ArchivosService", s);
+    }
+    catch(Exception e){
+      System.out.println("Error: " + e);
+    }
+  }  
+
+  public static void main(String [] args){
+    ArrayList<Usuario> usr = new ArrayList<Usuario>();
+    Usuario u;
+    try{
+      Autenticador a = (Autenticador)
+      Naming.lookup("rmi://127.0.0.1:21131/AutenticadorService");
+
+      usr = a.getUsuarios();
+      Iterator<Usuario> iterador = usr.iterator();
+
+     while(iterador.hasNext()){
+      u = iterador.next();
+      System.out.println("Encontrado: "+u.getUsuario()+" "+u.getContrasena());
+     }
+
+    }
+    catch (MalformedURLException murle) {
+      System.out.println();
+      System.out.println(
+        "MalformedURLException");
+      System.out.println(murle);
+    }
+    catch (RemoteException re) {
+      System.out.println();
+      System.out.println(
+        "RemoteException");
+      System.out.println(re);
+    }
+    catch (NotBoundException nbe) {
+      System.out.println();
+      System.out.println(
+       "NotBoundException");
+      System.out.println(nbe);
+    }
+    catch (
+      java.lang.ArithmeticException
+      ae) {
+      System.out.println();
+      System.out.println(
+       "java.lang.ArithmeticException");
+      System.out.println(ae);
+    }
+  }
+}
